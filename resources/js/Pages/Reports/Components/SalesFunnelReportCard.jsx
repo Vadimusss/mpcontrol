c4360a79464a25e6b2e62227186816ae5b1900c8
Vadim <v_a_d@inbox.ru>
2025-03-10 22:04:33 +0300

    set default start date to 30 days ago

--- resources/js/Pages/Reports/Components/SalesFunnelReportCard.jsx
+++ resources/js/Pages/Reports/Components/SalesFunnelReportCard.jsx
@@ -2,44 +2,63 @@ import React, { useState } from 'react';
 import PrimaryButton from '@/Components/PrimaryButton';
 
 export default function SalesFunnelReportCard({ shopId, report, goodLists, handleDownload }) {
-    const [selectedGoodListId, setSelectedGoodListId] = useState(
-        goodLists.length === 0 ? null : goodLists[0].id
-    );
+  const [selectedGoodListId, setSelectedGoodListId] = useState(
+    goodLists.length === 0 ? null : goodLists[0].id
+  );
 
-    const currentDate = new Date().toISOString().split('T')[0];
-    const [selectedBeginDate, setSelectedBeginDate] = useState(currentDate);
-    const [selectedEndDate, setSelectedEndDate] = useState(currentDate);
+  const currentDate = new Date().toISOString().split('T')[0];
+  const defaultBeginDate = new Date();
+  defaultBeginDate.setDate(defaultBeginDate.getDate() - 30);
+  const defaultBeginDateFormatted = defaultBeginDate.toISOString().split('T')[0];
 
-    return (
-        <div className="border border-gray-300 rounded-md shadow-sm bg-white mb-2 pt-2 pl-2 pb-2 pr-10">
-            <p className="mb-4"><span className='font-semibold text-gray-900'>Выгрузка:</span> {report.type.name}</p>
-            <div className="mb-4 flex gap-x-8">
-                <div className="mb-4 w-64">
-                    <p className="block mb-2 text-sm font-medium text-gray-900">{selectedGoodListId === null ? 'Создайте список товаров' : 'Выберите список товаров'}</p>
-                    <select
-                        onChange={e => setSelectedGoodListId(e.target.value)}
-                        className="mb-12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
-                        {goodLists && goodLists.map(({ id, name }) => <option value={id} key={id}>{name}</option>)}
-                    </select>
-                    <PrimaryButton
-                        className="max-w-fit"
-                        onClick={() => handleDownload(shopId, report, selectedBeginDate, selectedEndDate, selectedGoodListId)}
-                        disabled={!selectedGoodListId}>
-                        Скачать
-                    </PrimaryButton>
-                </div>
-                <div className="w-40">
-                    <label className="block mb-2 text-sm font-medium text-gray-900">Начальная дата:</label>
-                    <input type="date" value={selectedBeginDate} onChange={(e) => setSelectedBeginDate(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 mb-4"
-                    />
-                    <label className="block mb-2 text-sm font-medium text-gray-900">Конечная дата:</label>
-                    <input type="date" value={selectedEndDate} min={selectedBeginDate} max={currentDate} onChange={(e) =>
-                        setSelectedEndDate(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                    />
-                </div>
-            </div>
-        </div >
-    );
+  const [selectedBeginDate, setSelectedBeginDate] = useState(defaultBeginDateFormatted);
+  const [selectedEndDate, setSelectedEndDate] = useState(currentDate);
+
+  return (
+    <div className="bg-white border border-gray-300 rounded-md shadow-sm p-4 mb-2">
+      <p className="mb-4 font-semibold text-gray-900">
+        Выгрузка: <span className="font-normal">{report.type.name}</span>
+      </p>
+      <div className="flex gap-8 mb-4">
+        <div className="w-64">
+          <p className="block mb-2 text-sm font-medium text-gray-900">
+            {selectedGoodListId === null ? 'Создайте список товаров' : 'Выберите список товаров'}
+          </p>
+          <select
+            onChange={(e) => setSelectedGoodListId(e.target.value)}
+            className="w-full p-2 mb-4 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+          >
+            {goodLists && goodLists.map(({ id, name }) => (
+              <option value={id} key={id}>{name}</option>
+            ))}
+          </select>
+          <PrimaryButton
+            className="max-w-fit"
+            onClick={() => handleDownload(shopId, report, selectedBeginDate, selectedEndDate, selectedGoodListId)}
+            disabled={!selectedGoodListId}
+          >
+            Скачать
+          </PrimaryButton>
+        </div>
+        <div className="w-40">
+          <label className="block mb-2 text-sm font-medium text-gray-900">Начальная дата:</label>
+          <input
+            type="date"
+            value={selectedBeginDate}
+            onChange={(e) => setSelectedBeginDate(e.target.value)}
+            className="w-full p-2 mb-4 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+          />
+          <label className="block mb-2 text-sm font-medium text-gray-900">Конечная дата:</label>
+          <input
+            type="date"
+            value={selectedEndDate}
+            min={selectedBeginDate}
+            max={currentDate}
+            onChange={(e) => setSelectedEndDate(e.target.value)}
+            className="w-full p-2 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
