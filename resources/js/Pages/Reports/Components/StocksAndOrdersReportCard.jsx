85721e523878628f6e840a371c6de7ebcec7dcc4
Vadim <v_a_d@inbox.ru>
2025-03-10 22:05:01 +0300

    set default start date to 1 day ago

--- resources/js/Pages/Reports/Components/StocksAndOrdersReportCard.jsx
+++ resources/js/Pages/Reports/Components/StocksAndOrdersReportCard.jsx
@@ -2,33 +2,47 @@ import React, { useState } from 'react';
 import PrimaryButton from '@/Components/PrimaryButton';
 
 export default function StocksAndOrdersReportCard({ shopId, report, handleDownload }) {
-    const currentDate = new Date().toISOString().split('T')[0];
-    const [selectedBeginDate, setSelectedBeginDate] = useState(currentDate);
-    const [selectedEndDate, setSelectedEndDate] = useState(currentDate);
+  const currentDate = new Date().toISOString().split('T')[0];
+  const defaultBeginDate = new Date();
+  defaultBeginDate.setDate(defaultBeginDate.getDate() - 1);
+  const defaultBeginDateFormatted = defaultBeginDate.toISOString().split('T')[0];
+  const [selectedBeginDate, setSelectedBeginDate] = useState(defaultBeginDateFormatted);
+  const [selectedEndDate, setSelectedEndDate] = useState(currentDate);
 
-    return (
-        <div className="border border-gray-300 rounded-md shadow-sm bg-white mb-2 pt-2 pl-2 pb-2 pr-10">
-            <p className="mb-4"><span className='font-semibold text-gray-900'>Выгрузка:</span> {report.type.name}</p>
-            <div className="mb-4 flex gap-x-8 items-end">
-                    <PrimaryButton
-                        className="max-w-fit mr-auto"
-                        onClick={() => handleDownload(shopId, report, selectedBeginDate, selectedEndDate)}>
-                        Скачать
-                    </PrimaryButton>
-                <div className="w-40">
-                    <label className="block mb-2 text-sm font-medium text-gray-900">Начальная дата:</label>
-                    <input type="date" value={selectedBeginDate} onChange={(e) => setSelectedBeginDate(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                    />
-                </div>
-                <div className="w-40">
-                    <label className="block mb-2 text-sm font-medium text-gray-900">Конечная дата:</label>
-                    <input type="date" value={selectedEndDate} min={selectedBeginDate} max={currentDate} onChange={(e) =>
-                        setSelectedEndDate(e.target.value)}
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
-                    />
-                </div>
-            </div>
-        </div >
-    );
+  return (
+    <div className="bg-white border border-gray-300 rounded-md shadow-sm p-4 mb-2">
+      <p className="mb-4 font-semibold text-gray-900">
+        Выгрузка: <span className="font-normal">{report.type.name}</span>
+      </p>
+      <div className="flex items-end gap-8 mb-4">
+        <PrimaryButton
+          className="max-w-fit mr-auto"
+          onClick={() => handleDownload(shopId, report, selectedBeginDate, selectedEndDate)}
+        >
+          Скачать
+        </PrimaryButton>
+        <div className="w-40">
+          <label className="block mb-2 text-sm font-medium text-gray-900">Начальная дата:</label>
+          <input
+            type="date"
+            value={selectedBeginDate}
+            max={selectedEndDate}
+            onChange={(e) => setSelectedBeginDate(e.target.value)}
+            className="w-full p-2 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div className="w-40">
+          <label className="block mb-2 text-sm font-medium text-gray-900">Конечная дата:</label>
+          <input
+            type="date"
+            value={selectedEndDate}
+            min={selectedBeginDate}
+            max={currentDate}
+            onChange={(e) => setSelectedEndDate(e.target.value)}
+            className="w-full p-2 text-sm text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
