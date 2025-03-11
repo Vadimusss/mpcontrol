f94b891eeed8c2b0d691c9b355508faae973bed3
Vadim <v_a_d@inbox.ru>
2025-03-11 22:33:06 +0300

    update schedule for DailyWbApiDataUpdate task

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use App\Jobs\DailyWbApiDataUpdate;
 use App\Jobs\DailyShopsDataUpdate;
+use App\Jobs\MigrateSalesFunnelData;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -14,4 +15,20 @@ Schedule::job(new DailyShopsDataUpdate)->dailyAt('00:30');
 
 Schedule::job(new DailyWbApiDataUpdate(1))->dailyAt('01:00');
 
-// Schedule::job(new DailyWbAdvV1UpdUpdate, 'api')->everyMinute();
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('07:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('09:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('11:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('13:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('15:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('17:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('19:00');
+
+Schedule::job(new DailyWbApiDataUpdate())->dailyAt('21:00');
+
+// Schedule::job(new MigrateSalesFunnelData, 'api')->everyMinute();
