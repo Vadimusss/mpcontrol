76f9d3e29a362b149049d12d38d0a65aa96c3912
Vadim <v_a_d@inbox.ru>
2025-04-22 22:51:56 +0300

    fix relations update

--- app/Jobs/AddShopWbListGoods.php
+++ app/Jobs/AddShopWbListGoods.php
@@ -9,33 +9,31 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use App\Events\JobFailed;
 use Throwable;
-use Illuminate\Support\Facades\Log;
 
 class AddShopWbListGoods implements ShouldQueue
 {
     use Queueable;
 
+    private WbApiService $api;
+
     public function __construct(public Shop $shop, public $timeout = 600)
     {
         $this->shop = $shop->withoutRelations();
+        $this->api = new WbApiService($shop->apiKey->key);
     }
 
     public function handle(): void
     {
-        $shop = $this->shop;
-        $apiKey = $shop->apiKey->key;
+        $wbGoodListData = $this->api->getFullApiV2ListGoods();
 
-        $api = new WbApiService($apiKey);
-        $wbGoodListData = $api->getFullApiV2ListGoods();
-
-        $wbGoodListData->each(function ($goodFromApi) use ($shop) {
+        $wbGoodListData->each(function ($goodFromApi) {
             $existingByNmId = Good::with('wbListGoodRow')
-                ->where('shop_id', $shop->id)
+                ->where('shop_id', $this->shop->id)
                 ->where('nm_id', $goodFromApi['nmID'])
                 ->first();
 
             $existingByVendorCode = Good::with('wbListGoodRow')
-                ->where('shop_id', $shop->id)
+                ->where('shop_id', $this->shop->id)
                 ->where('vendor_code', $goodFromApi['vendorCode'])
                 ->where('nm_id', '!=', $goodFromApi['nmID'])
                 ->first();
@@ -47,40 +45,53 @@ class AddShopWbListGoods implements ShouldQueue
                 $existingByVendorCode->wbListGoodRow()->update([
                     'vendor_code' => $goodFromApi['vendorCode']
                 ]);
+                $this->updateGoodRelations($existingByVendorCode, $goodFromApi);
             } elseif ($existingByNmId) {
                 $existingByNmId->update(['vendor_code' => $goodFromApi['vendorCode']]);
-                $existingByNmId->wbListGoodRow()->update([
-                    'vendor_code' => $goodFromApi['vendorCode']
-                ]);
+                $this->updateGoodRelations($existingByNmId, $goodFromApi);
             } else {
-                $good = $shop->goods()->create([
-                    'shop_id' => $shop->id,
-                    'nm_id' => $goodFromApi['nmID'],
-                    'vendor_code' => $goodFromApi['vendorCode'],
-                ]);
-
-                $good->wbListGoodRow()->create([
-                    'good_id' => $good->id,
-                    'nm_id' => $goodFromApi['nmID'],
-                    'vendor_code' => $goodFromApi['vendorCode'],
-                    'currency_iso_code_4217' => $goodFromApi['currencyIsoCode4217'],
-                    'discount' => $goodFromApi['discount'],
-                    'club_discount' => $goodFromApi['clubDiscount'],
-                    'editable_size_price' => $goodFromApi['editableSizePrice'],
-                ]);
-
-                collect($goodFromApi['sizes'])->each(function ($size) use ($good) {
-                    $good->sizes()->create([
-                        'good_id' => $good->id,
-                        'size_id' => $size['sizeID'],
-                        'price' => $size['price'],
-                        'discounted_price' => $size['discountedPrice'],
-                        'club_discounted_price' => $size['clubDiscountedPrice'],
-                        'tech_size_name' => $size['techSizeName'],
-                    ]);
-                });
+                $this->createNewGood($this->shop, $goodFromApi);
             }
         });
+    }
+
+    private function updateGoodRelations(Good $good, array $goodFromApi): void
+    {
+        $good->wbListGoodRow()->delete();
+        $good->wbListGoodRow()->create([
+            'good_id' => $good->id,
+            'nm_id' => $goodFromApi['nmID'],
+            'vendor_code' => $goodFromApi['vendorCode'],
+            'currency_iso_code_4217' => $goodFromApi['currencyIsoCode4217'],
+            'discount' => $goodFromApi['discount'],
+            'club_discount' => $goodFromApi['clubDiscount'],
+            'editable_size_price' => $goodFromApi['editableSizePrice'],
+        ]);
+
+        $good->sizes()->delete();
+        collect($goodFromApi['sizes'])->each(function ($size) use ($good) {
+            $good->sizes()->create([
+                'good_id' => $good->id,
+                'size_id' => $size['sizeID'],
+                'price' => $size['price'],
+                'discounted_price' => $size['discountedPrice'],
+                'club_discounted_price' => $size['clubDiscountedPrice'],
+                'tech_size_name' => $size['techSizeName'],
+            ]);
+        });
+    }
+
+    private function createNewGood(Shop $shop, array $goodFromApi): Good
+    {
+        $good = $shop->goods()->create([
+            'shop_id' => $shop->id,
+            'nm_id' => $goodFromApi['nmID'],
+            'vendor_code' => $goodFromApi['vendorCode'],
+        ]);
+
+        $this->updateGoodRelations($good, $goodFromApi);
+        
+        return $good;
     }
 
     public function failed(?Throwable $exception): void
