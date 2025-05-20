f7921d0106683d20636e2eb3b003418cf7631cc2
Vadim <v_a_d@inbox.ru>
2025-05-20 21:39:37 +0300

    remove unnecessary methods

--- app/Policies/ShopPolicy.php
+++ app/Policies/ShopPolicy.php
@@ -9,30 +9,6 @@ use Illuminate\Auth\Access\Response;
 class ShopPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Shop $shop): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Shop $shop): bool
@@ -46,21 +22,5 @@ class ShopPolicy
     public function delete(User $user, Shop $shop): bool
     {
         return $this->update($user, $shop);
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Shop $shop): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Shop $shop): bool
-    {
-        //
     }
 }
