1e141d9d5b9e967ccf402319bd46aac6509720e0
Vadim <v_a_d@inbox.ru>
2025-06-17 22:14:25 +0300

    update customers header styling

--- resources/js/Pages/Shops/Components/Customers.jsx
+++ resources/js/Pages/Shops/Components/Customers.jsx
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/react';
 export default function Customers({ shopId, customers }) {
     return (
         <>
-            <p>Пользователи:</p>
+            <p className='font-semibold mt-2'>Пользователи:</p>
             < div className='flex border border-gray-300 rounded-md shadow-sm p-2 w-fit max-w-xl gap-2 flex-wrap bg-gray-100'>
                 {customers.map((customer) =>
                     <div key={customer.id} className='flex items-center gap-x-2 border border-gray-300 rounded-md px-2 py-1 bg-white'>
