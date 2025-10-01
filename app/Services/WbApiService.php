487aed4c441a3092b019d925c03cc6b216178645
Vadim <v_a_d@inbox.ru>
2025-10-01 21:40:02 +0300

    increase retry delay

--- app/Services/WbApiService.php
+++ app/Services/WbApiService.php
@@ -281,7 +281,7 @@ class WbApiService
         $response = Http::withToken($this->apiKey)
             ->timeout(90)
             ->connectTimeout(60)
-            ->retry(3, 1000)
+            ->retry(3, 60000)
             ->get('https://advert-api.wildberries.ru/adv/v3/fullstats', [
                 'ids' => implode(',', $ids),
                 'beginDate' => $beginDate,
